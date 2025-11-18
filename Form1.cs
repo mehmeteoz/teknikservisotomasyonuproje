c4a3cf0323196f13f68c6f564b6cc2d175f264cc
MehmetOz <146453831+mehmeteoz@users.noreply.github.com>
2025-11-18 20:52:55 +0300

    Revert "Kullanıcı Giriş yap arayüzü tamamlandı"
    This reverts commit 7bb73ee53a8d969ed8eb68147bf9af8846de0361.

--- Form1.cs
+++ Form1.cs
@@ -7,69 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 namespace TeknikServisOtomasyonuProje
 {
     public partial class Form1 : Form
     {
-        SQLConnect sqlConnect = new SQLConnect();
-        SqlConnection con;
         public Form1()
         {
             InitializeComponent();
-            con = sqlConnect.connectToSQL();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string id = textBox1.Text;
-
-            if (id.Trim() == "")
-            {
-                MessageBox.Show("Lütfen ID giriniz.");
-                return;
-            }
-
-            try
-            {
-                con.Open();
-
-                string query = "SELECT name, password FROM TestUser WHERE id = @id";
-
-                SqlCommand cmd = new SqlCommand(query, con);
-                cmd.Parameters.AddWithValue("@id", id);
-
-                SqlDataReader dr = cmd.ExecuteReader();
-
-                if (dr.Read())
-                {
-                    nameLabel.Text = dr["name"].ToString();
-                    passLabel.Text = dr["password"].ToString();
-                }
-                else
-                {
-                    MessageBox.Show("Bu ID'ye ait kayıt bulunamadı.");
-                    nameLabel.Text = "İsim";
-                    passLabel.Text = "Şifre";
-                }
-
-                dr.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Hata: " + ex.Message);
-            }
-            finally
-            {
-                if (con.State == ConnectionState.Open)
-                    con.Close();
-            }
         }
     }
 }
